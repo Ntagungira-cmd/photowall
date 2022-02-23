709ba200b3083848796dc47f6f6f73bd8344f8b6
Ntagungira-cmd <ntagungiraali@gmail.com>
2022-02-23 23:21:39 +0200

    redux toolkit

--- src/photowall/AddPhoto.js
+++ src/photowall/AddPhoto.js
@@ -1,47 +1,42 @@
 import React from "react";
-import {useNavigate} from 'react-router-dom'
-  
-  function AddPhoto(props) {
-    let navigate=useNavigate();
+import { useNavigate } from "react-router-dom";
 
-    const handlesubmit=(event) =>{
-      event.preventDefault();
-      const imageLink = event.target.elements.link.value;
-      const description = event.target.elements.description.value;
-  
-      const postadded = {
-        id: Number(new Date()),
-        description: description,
-        imageLink: imageLink,
-      };
-      if (imageLink && description) {
-        props.prop.addPhoto(postadded);
-        return navigate("/");
-      }
+function AddPhoto(props) {
+  let navigate = useNavigate();
+
+  const handlesubmit = (event) => {
+    event.preventDefault();
+    const imageLink = event.target.elements.link.value;
+    const description = event.target.elements.description.value;
+
+    const postadded = {
+      id: Number(new Date()),
+      description: description,
+      imageLink: imageLink,
+    };
+    if (imageLink && description) {
+      props.prop.addPhoto(postadded);
+      return navigate("/");
     }
-    console.log(props);
-    return (
-      <div>
-        <h1>Photowall</h1>
-        <div className="form">
-          <form onSubmit={handlesubmit}>
-            <input
-              className="input"
-              type="text"
-              placeholder="Link"
-              name="link"
-            />
-            <input
-              className="input"
-              type="text"
-              placeholder="Description"
-              name="description"
-            />
-            <button className="formbutton"> Post </button>
-          </form>
-        </div>
+  };
+  //console.log(props);
+  return (
+    <div>
+      <h1>Photowall</h1>
+      <div className="form">
+        <form onSubmit={handlesubmit}>
+          <input className="input" type="text" placeholder="Link" name="link" />
+          <input
+            className="input"
+            type="text"
+            placeholder="Description"
+            name="description"
+          />
+          <button className="formbutton"> Post </button>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default AddPhoto;
